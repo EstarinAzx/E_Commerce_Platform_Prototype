fd7c4ed9621ce86b740bb1930a743e97e539a99e
EstarinAzx <khitarashi@gmail.com>
2025-11-29 00:08:53 +1300

    mobile sidebar fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -72,7 +72,7 @@ export default function Layout({ children }: LayoutProps) {
             <div className="flex h-screen overflow-hidden">
                 {/* Sidebar */}
                 <aside className={cn(
-                    "fixed inset-y-0 left-0 z-50 w-64 bg-card border-r transform transition-transform duration-200 ease-in-out lg:relative lg:translate-x-0",
+                    "fixed inset-y-0 left-0 z-50 w-64 bg-background border-r shadow-lg transform transition-transform duration-200 ease-in-out lg:relative lg:translate-x-0",
                     isMobileMenuOpen ? "translate-x-0" : "-translate-x-full"
                 )}>
                     <div className="flex flex-col h-full">
